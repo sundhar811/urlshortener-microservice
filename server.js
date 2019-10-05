111880a853603ece2a78d11493ffdfadc1563e08
Glitch (sundhar811-urlshortener-microservice) <none>
2019-10-05 06:49:30 +0000

    connected mongodb atlas cluster

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 
